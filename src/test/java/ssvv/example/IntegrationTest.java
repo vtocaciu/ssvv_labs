d4ec42f8662854271172d989a29babad6544d131
Valeria <valeria.tocaciu@gmail.com>
2021-04-26 21:46:27 +0300

    increment testing

--- src/test/java/ssvv/example/IntegrationTest.java
+++ src/test/java/ssvv/example/IntegrationTest.java
@@ -46,4 +46,43 @@ public class IntegrationTest {
         TestCase.assertEquals(size1+1, size2);
     }
 
+    @Test
+    public void saveStudent_Increment() {
+        int size1 = service.getStudentSize();
+        service.saveStudent("1", "nume", 932, "mail@mail.com", "nume");
+        int size2 = service.getStudentSize();
+        TestCase.assertEquals(size1+1, size2);
+    }
+
+    @Test
+    public void saveTema_Increment() {
+        int sizeStudent1 = service.getStudentSize();
+        service.saveStudent("2", "nume2", 937, "mail2@mail.com", "nume2");
+        int sizeStudent2 = service.getStudentSize();
+        TestCase.assertEquals(sizeStudent1+1, sizeStudent2);
+
+        int sizeTema1 = service.getTemaSize();
+        service.saveTema("1", "desc", 5, 3);
+        int sizeTema2 = service.getTemaSize();
+        TestCase.assertEquals(sizeTema1+1, sizeTema2);
+    }
+
+    @Test
+    public void saveNota_Increment() {
+        int sizeStudent1 = service.getStudentSize();
+        service.saveStudent("3", "nume3", 935, "mail3@mail.com", "nume3");
+        int sizeStudent2 = service.getStudentSize();
+        TestCase.assertEquals(sizeStudent1+1, sizeStudent2);
+
+        int sizeTema1 = service.getTemaSize();
+        service.saveTema("2", "desc2", 7, 4);
+        int sizeTema2 = service.getTemaSize();
+        TestCase.assertEquals(sizeTema1+1, sizeTema2);
+
+        int sizeNota1 = service.getNotaSize();
+        service.saveNota("3", "2", 5, 5, "okay");
+        int sizeNota2 = service.getNotaSize();
+        TestCase.assertEquals(sizeNota1+1, sizeNota2);
+    }
+
 }
